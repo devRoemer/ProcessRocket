2547bca4bd649ec1d14c220bb7b3a0102e02edb8
Stefan Rom <srom@samhammer.de>
2021-08-04 20:31:52 +0200

    Create installer

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,32 @@ module.exports = {
       mainProcessFile: "./src/electron/Entrypoint.ts",
       nodeIntegration: true,
       externals: ["node-pty"],
+      builderOptions: {
+        // Recommendation to be set for nsis according to documentation
+        appId: "de.devroemer.processrocket",
+        extraMetadata: {
+          // Used for directory names in AppData\local and AppData\local\Programs
+          name: "ProcessRocket",
+          // Used for directory names in AppData\Roaming
+          productName: "ProcessRocket",
+        },
+        // Used as a display name
+        productName: "ProcessRocket",
+        directories: {
+          output: "./setup",
+        },
+        extraResources: ["./src/assets/**"],
+        win: {
+          icon: "./public/favicon.ico",
+          target: [
+            {
+              target: "nsis",
+
+              arch: ["x64"],
+            },
+          ],
+        },
+      },
     },
   },
 };
